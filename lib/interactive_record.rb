b173b8212346fd6a8661d10f1604c750e65844d1
Jayon Kim <jpkim921@gmail.com>
2018-05-06 20:03:58 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -12,11 +12,17 @@ class InteractiveRecord
 
     table_info = DB[:conn].execute(sql)
     column_names = []
-    
+
     table_info.each do |row|
       column_names << row["name"]
     end
     column_names.compact
+  end
+
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
   end
 
 end
